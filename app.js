9ed0f612f0cc915e50e1394a8751fcb4ceb4ba3b
Alton Chaney <alton@insito.me>
2018-03-04 19:58:25 -0600

    - hmm thinking this might be causing an issue in heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-require('dotenv').config();
+// require('dotenv').config();
 
 var app = express();
 
