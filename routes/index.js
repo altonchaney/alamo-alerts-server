c2583d18086e82a9f5027cb686e83b8b804ec0fb
Alton Chaney <alton@ruckit.me>
2021-04-03 18:38:46 -0500

    refactor route setup. add better error handling

--- routes/index.js
+++ routes/index.js
@@ -3,45 +3,56 @@ var twit = require('twitter');
 var router = express.Router();
 var path = require('path');
 
-var twitter = new twit({
+const twitter = new twit({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
-router.get('/tweets/village', function(req, res, next) {
-  twitter.get('statuses/user_timeline', { screen_name: "AlamoAlerts_003", count: "6" }, function(error, tweets, response){
-    if(error) throw error;
-    res.send(tweets);
-  });
-});
+const supportedMarkets = [
+  {
+    url: '/tweets/village',
+    params: {
+      screen_name: 'AlamoAlerts_003',
+      count: '6'
+    }
+  }, {
+    url: '/tweets/south-lamar',
+    params: {
+      screen_name: 'AlamoAlerts_004',
+      count: '6'
+    }
+  }, {
+    url: '/tweets/slaughter-lane',
+    params: {
+      screen_name: 'AlamoAlerts_006',
+      count: '6'
+    }
+  }, {
+    url: '/tweets/lakeline',
+    params: {
+      screen_name: 'AlamoAlerts_007',
+      count: '6'
+    }
+  }, {
+    url: '/tweets/mueller',
+    params: {
+      screen_name: 'AlamoAlerts_008',
+      count: '6'
+    }
+  }
+];
 
-router.get('/tweets/south-lamar', function(req, res, next) {
-  twitter.get('statuses/user_timeline', { screen_name: "AlamoAlerts_004", count: "6" }, function(error, tweets, response){
-    if(error) throw error;
-    res.send(tweets);
-  });
-});
-
-router.get('/tweets/slaughter-lane', function(req, res, next) {
-  twitter.get('statuses/user_timeline', { screen_name: "AlamoAlerts_006", count: "6" }, function(error, tweets, response){
-    if(error) throw error;
-    res.send(tweets);
-  });
-});
-
-router.get('/tweets/lakeline', function(req, res, next) {
-  twitter.get('statuses/user_timeline', { screen_name: "AlamoAlerts_007", count: "6" }, function(error, tweets, response){
-    if(error) throw error;
-    res.send(tweets);
-  });
-});
-
-router.get('/tweets/mueller', function(req, res, next) {
-  twitter.get('statuses/user_timeline', { screen_name: "AlamoAlerts_008", count: "6" }, function(error, tweets, response){
-    if(error) throw error;
-    res.send(tweets);
+supportedMarkets.forEach(market => {
+  router.get(market.url, function(req, res, next) {
+    twitter.get('statuses/user_timeline', market.params, function(error, tweets, response){
+      if (error) {
+        res.status(response.statusCode).json({ error: error });
+      } else {
+        res.send(tweets);
+      };
+    });
   });
 });
 
